f542e52a0a4be050215d682bb38b7451707a7b31
Tom Plucinski <tapluci1@gmail.com>
2020-11-22 23:18:50 -0600

    Initial commit

---
+++ src/main/java/com/project/covid19dashboard/Covid19dashboardApplication.java
@@ -0,0 +1,13 @@
+package com.project.covid19dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19dashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19dashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/covid19dashboard/Covid19dashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.covid19dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19dashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
